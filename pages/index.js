dd0df75b4ac2ea33d5414eab36c70a8aaa86baf3
Tanakorn Rattanajariya <lufas2603@gmail.com>
2021-01-15 22:46:04 +0700

    First Init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card } from "antd";
+import stylesheet from "../styles/index.less";
+import Main from "./Main";
+export default function Home() {
+  return (
+    <Main>
+      <Card title="wow">Test</Card>
+    </Main>
+  );
+}
