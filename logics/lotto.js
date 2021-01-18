37324d25b4798290505aa416d836bc0761d0b44a
Tanakorn Rattanajariya <lufas2603@gmail.com>
2021-01-18 15:07:38 +0700

    Added new lotto logic

---
+++ logics/lotto.js
@@ -0,0 +1,15 @@
+/**
+ * @desc Set winning prize to object
+ * @param { { date:string,endpoint:string,prizes:[ { amount,id,name,reward,number:[string]}],runningNumbers: [{ amount,id,name,reward,number:[string]}] }} result
+ * @param {string} value
+ * @return { { key:{ amount,id,name,number,reward} } }
+ */
+function setWinningPrize(result, value) {
+  return (result?.prizes || [])
+    .filter((v) => (v.number || []).filter((e) => e === value).length > 0)
+    .reduce((acc, v) => {
+      return { ...acc, [v.id]: v };
+    }, {});
+}
+
+export { setWinningPrize };
