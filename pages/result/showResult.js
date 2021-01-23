8b3107a3244d18f6e421a94b77a7fa9379ab6139
Tanakorn Rattanajariya <lufas2603@gmail.com>
2021-01-23 17:09:39 +0700

    Fixed

--- pages/result/showResult.js
+++ pages/result/showResult.js
@@ -1,5 +1,5 @@
 import { Row, Col, Card, Typography, Divider, Spin } from "antd";
-import { setResultEqual,setResultNotEqual } from "logics/lotto";
+import { setResultEqual, setResultNotEqual } from "logics/lotto";
 const { Title, Text } = Typography;
 
 const styles = {
@@ -28,15 +28,14 @@ const styles = {
 
 export default function ResultShowResult(props) {
   const { loading_result } = props.reducer.component;
-  const {result} = props?.reducer?.lotto;
-  var firstPrize = setResultEqual(result,"prizes", "prizeFirst");
-  var otherTier = setResultNotEqual(result,"prizes", "prizeFirst");
-  var runningNumbers = setResultNotEqual(result,"runningNumbers",null);
+  const { result } = props?.reducer?.lotto;
+  var firstPrize = setResultEqual(result, "prizes", "prizeFirst");
+  var otherTier = setResultNotEqual(result, "prizes", "prizeFirst");
+  var runningNumbers = setResultNotEqual(result, "runningNumbers", null);
   return (
     <Card style={{ marginTop: 16 }}>
-      
-      <Random1/>
-      <Random2/>
+      <Random1 />
+      <Random2 />
       {loading_result ? (
         <Spin style={{ display: "block", margin: "auto" }} />
       ) : (
@@ -51,39 +50,37 @@ export default function ResultShowResult(props) {
   );
 }
 
-function FirstTier({firstTier}) {
+function FirstTier({ firstTier }) {
   return (
     <>
       {firstTier &&
         (Object.keys(firstTier) || [])
-        .sort((a, b) => firstTier[a].amount - firstTier[b].amount)
-        .map((v, i) => {
-          return (
-            <Col
-              key={v}
-              style={styles[firstTier[v].id].style || {}}
-              span={styles[firstTier[v].id].span || 24}
-            >
-              <Row key={i} justify='center' gutter={12}>
-                {(firstTier[v].number || []).map((e, i) => {
-                  return (
-                      <Col key={e} xs={12} sm={8} lg={5} >
-                        <Text type={styles[firstTier[v].id].textType}>
-                          {e}
-                        </Text>
-                      </Col>
-                  );
-                })}
-              </Row>
-              <Title
-                level={styles[firstTier[v].id].titleLevel || 0}
-                style={{ marginTop: "0px" }}
+          .sort((a, b) => firstTier[a].amount - firstTier[b].amount)
+          .map((v, i) => {
+            return (
+              <Col
+                key={v}
+                style={styles[firstTier[v].id].style || {}}
+                span={styles[firstTier[v].id].span || 24}
               >
-                {firstTier[v].name}
-              </Title>
-            </Col>
-          );
-        })}
+                <Row key={i} justify="center" gutter={12}>
+                  {(firstTier[v].number || []).map((e, i) => {
+                    return (
+                      <Col key={e} xs={12} sm={8} lg={5}>
+                        <Text type={styles[firstTier[v].id].textType}>{e}</Text>
+                      </Col>
+                    );
+                  })}
+                </Row>
+                <Title
+                  level={styles[firstTier[v].id].titleLevel || 0}
+                  style={{ marginTop: "0px" }}
+                >
+                  {firstTier[v].name}
+                </Title>
+              </Col>
+            );
+          })}
     </>
   );
 }
@@ -113,25 +110,36 @@ function OtherTier({ otherTier }) {
   });
 }
 
-
-function Random1(){
+function Random1() {
   var numbers = {};
-  for(var i =0;i<9999;i++){
-    var num =Math.floor(Math.random() * 100);
-    numbers[num] = (numbers[num] || 0)+1;
+  for (var i = 0; i < 1000000; i++) {
+    var num = Math.floor(Math.random() * 100);
+    numbers[num] = (numbers[num] || 0) + 1;
   }
-  const mostFrequent = Object.keys(numbers).sort((a,b)=>numbers[b]-numbers[a])[0];
-  return(<Title>1 prediction number  {mostFrequent}</Title>)
+  const mostFrequent = Object.keys(numbers).sort(
+    (a, b) => numbers[b] - numbers[a]
+  )[0];
+  const lastFrequent = Object.keys(numbers)
+    .sort((a, b) => numbers[b] - numbers[a])
+    .last();
+  return (
+    <Title>
+      1 prediction number {mostFrequent} {numbers[mostFrequent]}{" "}
+      {numbers[lastFrequent]}
+    </Title>
+  );
 }
 
-function Random2(){
+function Random2() {
   var numbers = {};
-  for(var i =0;i<9999;i++){
-    var first =Math.floor(Math.random() * 10);
+  for (var i = 0; i < 1000000; i++) {
+    var first = Math.floor(Math.random() * 10);
     var second = Math.floor(Math.random() * 10);
     var sumString = first.toString() + second.toString();
-    numbers[sumString] = (numbers[sumString] || 0)+1;
+    numbers[sumString] = (numbers[sumString] || 0) + 1;
   }
-  const mostFrequent = Object.keys(numbers).sort((a,b)=>numbers[b]-numbers[a])[0];
-  return(<Title>2 prediction number  {mostFrequent}</Title>)
+  const mostFrequent = Object.keys(numbers).sort(
+    (a, b) => numbers[b] - numbers[a]
+  )[0];
+  return <Title>2 prediction number {mostFrequent}</Title>;
 }
